545b6dfdf59359eb8847ba44a5ef941b697ae208
Caio <caiofasoares@gmail.com>
2022-08-17 08:16:40 -0300

    Initial Commit

---
+++ Whatsapp2/Whatsapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  Whatsapp2App.swift
+//  Whatsapp2
+//
+//  Created by Caio Soares on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Whatsapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
